51f1cbd1e4a349c0750a650953d4a89aabb4bc37
virginia <mavirginia.cordero@gmail.com>
2018-09-10 10:40:04 -0400

    created user and group entities

---
+++ src/main/java/co/grandcricus/challengeaccepted/entity/Group.java
@@ -0,0 +1,45 @@
+package co.grandcricus.challengeaccepted.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Group {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	@OneToMany(mappedBy="group")
+	private Set<Challenge> challenges;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Set<Challenge> getChallenges() {
+		return challenges;
+	}
+	public void setChallenges(Set<Challenge> challenges) {
+		this.challenges = challenges;
+	}
+	
+}
--- src/main/java/co/grandcricus/challengeaccepted/entity/User.java
+++ src/main/java/co/grandcricus/challengeaccepted/entity/User.java
@@ -1,5 +1,69 @@
 package co.grandcricus.challengeaccepted.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private Integer priceMin;
+	private Integer priceMax;
+	private String location;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getPriceMin() {
+		return priceMin;
+	}
+	public void setPriceMin(Integer priceMin) {
+		this.priceMin = priceMin;
+	}
+	public Integer getPriceMax() {
+		return priceMax;
+	}
+	public void setPriceMax(Integer priceMax) {
+		this.priceMax = priceMax;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
 }
