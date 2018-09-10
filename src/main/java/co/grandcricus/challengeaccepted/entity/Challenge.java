1fc91e2922b6bab6b0889ff40bf1626189f2a48a
sharde <sharde.m.smith@gmail.com>
2018-09-10 10:41:46 -0500

    group entity

---
+++ src/main/java/co/grandcricus/challengeaccepted/entity/Challenge.java
@@ -0,0 +1,82 @@
+package co.grandcricus.challengeaccepted.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Challenge {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String name;
+	private String description;
+	private String location;
+	private LocalDate creationDate;
+	private LocalDate expirationDate;
+	private String type;
+
+	@ManyToOne
+	private Group group;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+	
+}
