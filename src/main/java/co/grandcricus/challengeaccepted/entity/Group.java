1d2155d18a8cf540b9a5603c69bc95f36cdcd922
EDWARD CARRINGTON <E_DIDDY@House-Of-Essex.local>
2018-09-10 11:32:41 -0400

    Added annotations for user and group classes.

--- src/main/java/co/grandcricus/challengeaccepted/entity/Group.java
+++ src/main/java/co/grandcricus/challengeaccepted/entity/Group.java
@@ -6,40 +6,62 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Group {
-	
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
-	@OneToMany(mappedBy="group")
+
+	@OneToMany(mappedBy = "group")
 	private Set<Challenge> challenges;
+
+	@ManyToMany(mappedBy = "groups")
+	private Set<User> users;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Set<Challenge> getChallenges() {
 		return challenges;
 	}
+
 	public void setChallenges(Set<Challenge> challenges) {
 		this.challenges = challenges;
 	}
-	
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+
 }
--- src/main/java/co/grandcricus/challengeaccepted/entity/User.java
+++ src/main/java/co/grandcricus/challengeaccepted/entity/User.java
@@ -1,14 +1,20 @@
 package co.grandcricus.challengeaccepted.entity;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
-	
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
@@ -17,53 +23,82 @@ public class User {
 	private Integer priceMin;
 	private Integer priceMax;
 	private String location;
+
+	@ManyToMany
+	@JoinTable(name = "user_group", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "group_id") })
+	private Set<Group> groups;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Integer getPriceMin() {
 		return priceMin;
 	}
+
 	public void setPriceMin(Integer priceMin) {
 		this.priceMin = priceMin;
 	}
+
 	public Integer getPriceMax() {
 		return priceMax;
 	}
+
 	public void setPriceMax(Integer priceMax) {
 		this.priceMax = priceMax;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
+	}
+
+	public Set<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(Set<Group> groups) {
+		this.groups = groups;
 	}
 
 }
