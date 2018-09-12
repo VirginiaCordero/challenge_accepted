1efd16cb539d7d86953fa186b553012baaadd8cd
EDWARD CARRINGTON <E_DIDDY@House-Of-Essex.local>
2018-09-12 15:34:08 -0400

    Edited dashboard method within Member Controller.

--- src/main/java/co/grandcircus/challengeaccepted/MemberController.java
+++ src/main/java/co/grandcircus/challengeaccepted/MemberController.java
@@ -1,8 +1,5 @@
 package co.grandcircus.challengeaccepted;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,122 +23,120 @@ import co.grandcircus.challengeaccepted.entity.User;
 import co.grandcircus.challengeaccepted.entity.UserChallenge;
 import co.grandcircus.challengeaccepted.model.googleplaces.PlaceDetailResult;
 
-
 @Controller
 public class MemberController {
-	
+
 	@Value("${google.api_key}")
 	private String apiKey;
-	
+
 	@Autowired
 	private GroupDao groupDao;
-	
+
 	@Autowired
 	private UserDao userDao;
-	
+
 	@Autowired
 	private ChallengeDao challengeDao;
-	
+
 	@Autowired
 	private UserChallengeDao userChallengeDao;
-	
+
 	@RequestMapping("/dashboard")
-	public ModelAndView showDashboard(@SessionAttribute(name="user", required=false) User user, RedirectAttributes redir) {
+	public ModelAndView showDashboard(@SessionAttribute(name = "user", required = false) User user,
+			RedirectAttributes redir) {
 		// For this URL, make sure there is a user on the session.
 		if (user == null) {
 			// if not, send them back to the login page with a message.
 			redir.addFlashAttribute("message", "Wait! Please log in.");
 			return new ModelAndView("redirect:/login");
 		}
-		
+
 		// if the user is logged in, proceed as normal.
 		ModelAndView mav = new ModelAndView("dashboard");
 		mav.addObject("groups", groupDao.findAll());
-		
+
 		Challenge nextChallenge = challengeDao.findFirstByGroupInOrderByCreationDateAsc(user.getGroups());
-		
-		RestTemplate restTemplate = new RestTemplate();
-		
-		String url = "https://maps.googleapis.com/maps/api/place/details/json?" +
-					 "placeid=" + nextChallenge.getPlaceId() +
-					 "&key=" + apiKey;
-		
-		System.out.println(url);
-		
-		PlaceDetailResult placeDetailResult = restTemplate.getForObject(url, PlaceDetailResult.class);
-		
+
+		PlaceDetailResult placeDetailResult = null;
+
+		if (nextChallenge != null) {
+
+			RestTemplate restTemplate = new RestTemplate();
+
+			String url = "https://maps.googleapis.com/maps/api/place/details/json?" + "placeid="
+					+ nextChallenge.getPlaceId() + "&key=" + apiKey;
+
+			System.out.println(url);
+
+			placeDetailResult = restTemplate.getForObject(url, PlaceDetailResult.class);
+
+		}
+
 		mav.addObject("nextChallengeDetails", placeDetailResult);
 		mav.addObject("nextChallenge", nextChallenge);
-	
+
 		return mav;
 	}
-	
+
 	@PostMapping("/dashboard/join-group")
-	public ModelAndView joinGroupFromDashboard(@SessionAttribute(name="user", required=false) User user,
-											   Group group,
-											   HttpSession session,
-											   RedirectAttributes redir) {
+	public ModelAndView joinGroupFromDashboard(@SessionAttribute(name = "user", required = false) User user,
+			Group group, HttpSession session, RedirectAttributes redir) {
 		// For this URL, make sure there is a user on the session.
 		if (user == null) {
 			// if not, send them back to the login page with a message.
 			redir.addFlashAttribute("message", "Wait! Please log in.");
 			return new ModelAndView("redirect:/login");
 		}
-		
+
 		User dbUser = userDao.findById(user.getId()).orElse(null);
 		dbUser.getGroups().add(group);
 		userDao.save(dbUser);
 		session.setAttribute("user", dbUser);
-		
+
 		redir.addFlashAttribute("You have been added to " + group.getName() + "!");
 		return new ModelAndView("redirect:/dashboard");
 	}
-	
+
 	@PostMapping("/dashboard/leave-group")
-	public ModelAndView leaveGroupFromDashboard(@SessionAttribute(name="user", required=false) User user,
-											   Group group,
-											   HttpSession session,
-											   RedirectAttributes redir) {
+	public ModelAndView leaveGroupFromDashboard(@SessionAttribute(name = "user", required = false) User user,
+			Group group, HttpSession session, RedirectAttributes redir) {
 		// For this URL, make sure there is a user on the session.
 		if (user == null) {
 			// if not, send them back to the login page with a message.
 			redir.addFlashAttribute("message", "Wait! Please log in.");
 			return new ModelAndView("redirect:/login");
 		}
-		
+
 		User dbUser = userDao.findById(user.getId()).orElse(null); // new managed entity "circle"
 		dbUser.getGroups().remove(group);
 		userDao.save(dbUser);
 		session.setAttribute("user", dbUser);
-		
+
 		return new ModelAndView("redirect:/dashboard");
 	}
-	
+
 	@RequestMapping("/challenge-response")
-	public ModelAndView acceptOrDeclineChallenge(@SessionAttribute(name="user", required=false) User user,
-												 @RequestParam("challengeId") Challenge challenge,
-												 @RequestParam("response") String response) {
-		
+	public ModelAndView acceptOrDeclineChallenge(@SessionAttribute(name = "user", required = false) User user,
+			@RequestParam("challengeId") Challenge challenge, @RequestParam("response") String response) {
+
 		// Row in the user_challenge Table
 		UserChallenge userChallenge = new UserChallenge();
-		
+
 		// Set all the values
 		userChallenge.setChallenge(challenge); // sets challenge_id
 		userChallenge.setUser(userDao.findById(user.getId()).orElse(null)); // sets user_id
 		userChallenge.setResponseDate(System.currentTimeMillis()); // sets responseDate
-		
+
 		if (response.equalsIgnoreCase("accepted")) {
 			userChallenge.setStatus("accepted"); // set status
 		} else {
 			userChallenge.setStatus("declined"); // set status
 		}
-		
+
 		// Save the Row
 		userChallengeDao.save(userChallenge);
-			
-		
+
 		return new ModelAndView("redirect:/dashboard");
 	}
-	
-	
+
 }
